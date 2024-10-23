2c2229803d054790e9d333975185d905e05b9334
Funan Zhou <matrix@blocksec.com>
2024-10-23 16:19:02 +0800

    add: support for None type

--- evm_decoder/utils/hex_utils.py
+++ evm_decoder/utils/hex_utils.py
@@ -1,6 +1,8 @@
 import binascii
 
 def ensure_hex_string(value):
+    if value is None:
+        return None
     if isinstance(value, str):
         return value if value.startswith('0x') else '0x' + value
     elif isinstance(value, bytes):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="evm-decoder",
-    version="0.1.7",
+    version="0.1.8",
     author="gmatrixuniverse",
     author_email="gmatrixuniverse@gmail.com",
     description="A package for decoding and analyzing EVM transactions and logs",
