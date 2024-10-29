237c5c9dd3a8eac0356eb5e6f77fa891b0ea46f2
Funan Zhou <matrix@blocksec.com>
2024-10-29 14:57:16 +0800

    update: add patterns

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="evm-decoder",
-    version="0.1.8",
+    version="0.1.9",
     author="gmatrixuniverse",
     author_email="gmatrixuniverse@gmail.com",
     description="A package for decoding and analyzing EVM transactions and logs",
